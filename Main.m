ee4779b382004820947ac12bece4d1170402fcf7
Mohammad Reza Mohebbian <van_mohebian@yahoo.com>
2019-03-04 17:26:49 -0600

    Add Deep Learning

--- Main.m
+++ Main.m
@@ -4,23 +4,23 @@ run('addToolbox.m')
 % %% Load Data
 I_person=LoadData_Slices();
 %% Add motion artifact
-noiseBasePars ={1.^[0:8],2.*[0:8],3.*[0:8],4.*[0:8]}; %% minimal,mild,moderate,severe motion
-maxDisp=[1,4,9,14];
-maxRot=[1,4,9,14];
-show=1;
-for i=1:numel(I_person)
-    for m=1:4
-        [image_simMotion{i,m},fitpars{i,m}]=addMotionArtifactToMRI(I_person{i},noiseBasePars{m},maxDisp(m),maxRot(m),show);
-    end
-    disp(['Person number ' num2str(i) ' is done...'])
-end
+% noiseBasePars ={.5.^[0:8],2.*[0:8],3.*[0:8],4.*[0:8]}; %% minimal,mild,moderate,severe motion
+% maxDisp=[1,4,9,14];
+% maxRot=[1,4,9,14];
+% show=1;
+% for i=1:numel(I_person)
+%     for m=1:4
+%         [image_simMotion{i,m},fitpars{i,m}]=addMotionArtifactToMRI(I_person{i},noiseBasePars{m},maxDisp(m),maxRot(m),show);
+%     end
+%     disp(['Person number ' num2str(i) ' is done...'])
+% end
 %% load images that are saved
-% load('simulatedImages.mat')
+load('simulatedImages.mat')
 %% showing add motion artifact
-m=1;
-addMotionArtifactToMRI(I_person{3},noiseBasePars{m},maxDisp(m),maxRot(m),1);
+% m=1;
+% addMotionArtifactToMRI(I_person{3},noiseBasePars{m},maxDisp(m),maxRot(m),1);
  % imshowDifferentMotion(I_person,image_simMotion,1,40)
 %% non:0 slight:1 mild:2 moderate:3 severe:4 
-Strategy1_FeatureSelectionMachineLearning
-%%
-
+% Strategy1_FeatureSelectionMachineLearning
+%% non:0 slight:1 mild:2 moderate:3 severe:4 
+Strategy2_DeepLearning
---
+++ Strategy2_DeepLearning.m
@@ -0,0 +1,64 @@
+%% non:0 slight:1 mild:2 moderate:3 severe:4
+%% Foldering
+imagesDir='DataFolderForDNN';
+originalFileLocation = fullfile(imagesDir);
+%% Save Original Image
+% For original images
+for i=1:numel(I_person)
+    for n=1:size(I_person{i},3)
+     imwrite(abs(I_person{i}(:,:,n)),[originalFileLocation '/M0/'  num2str(i) '_' num2str(n) '.png']);
+    end
+    disp(['Person number ' num2str(i) ' is done...'])
+end
+
+% For motion artifact
+for i=1:size(image_simMotion,1)
+    for j=1:size(image_simMotion,2)
+        for n=1:size(image_simMotion{i,j},3)
+            imwrite(abs(image_simMotion{i,j}(:,:,n)),[originalFileLocation '/M' num2str(j) '/'  num2str(i) '_' num2str(n) '.png']);      
+        end
+    end
+    disp(['Person number ' num2str(i) ' is done...'])
+end
+%% Load + TrainTest
+imds = imageDatastore(originalFileLocation, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames');
+TrainPercent=.7;
+numTrainingFiles =  round(numel(imds.Labels)/5*TrainPercent);
+[imdsTrain,imdsTest] = splitEachLabel(imds,numTrainingFiles,'randomize');
+%%
+layers = [
+    imageInputLayer([256 256 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(5)
+    softmaxLayer
+    classificationLayer];
+
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',20,...
+    'InitialLearnRate',1e-4, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+net = trainNetwork(imdsTrain,layers,options);
+%% predict
+YPred = classify(net,imdsTest);
+YTest = imdsTest.Labels;
+accuracy = sum(YPred == YTest)/numel(YTest)
+confusionmat(YTest,YPred)
